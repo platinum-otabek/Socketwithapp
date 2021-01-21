34ac6a25c96ffb9cef337c39f4db5406fe494bf1
platinum-otabek <mr.tursinboev@gmail.com>
2021-01-21 16:08:10 +0500

    2 3

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-const express = require('express')
-const path = require('path')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.set('port', ( 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-console.log("outside io");
-
-io.on('connection', function(socket){
-
-  console.log('User Conncetion');
-
-  socket.on('connect user', function(user){
-    console.log("Connected user ");
-    io.emit('connect user', user);
-  });
-
-  socket.on('on typing', function(typing){
-    console.log("Typing.... ");
-    io.emit('on typing', typing);
-  });
-
-  socket.on('chat message', function(msg){
-    console.log("Message " + msg['message']);
-    io.emit('chat message', msg);
-  });
+app.get('/', function(req, res) {
+    res.render('index.ejs');
 });
 
-http.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
 });
