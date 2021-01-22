96902106cc4180eb19fb90663e2d3b414afc40eb
platinum-otabek <mr.tursinboev@gmail.com>
2021-01-22 10:22:19 +0500

    ip

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
-io.sockets.on('connection', function(socket) {
+io.on('connection', function(socket) {
     socket.on('connect user', function(username) {
-        socket.connect_user = username;
-        io.emit('is_online', '🔵 <i>' + socket.connect_user + ' join the chat..</i>');
+       socket.connect_user = username;
+        io.emit('connect user', '🔵 <i>' + username + ' join the chat..</i>');
     });
     socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        io.emit('connect user', '🔴 <i>' + socket.connect_user + ' left the chat..</i>');
     })
-    socket.on('chat message', function(message) {
-        io.emit('chat message', '<strong>' + socket.username + '</strong>: ' + message);
+    socket.on('chat message', function(msg) {
+        console.log(msg);
+        io.emit('chat message', '<strong>' + socket.connect_user + '</strong>: ' + msg);
     });
 
 });
-
 const server = http.listen(8080, function() {
     console.log('listening on *:8080');
 });
